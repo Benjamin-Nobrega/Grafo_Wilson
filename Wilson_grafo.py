167209565f282a7220b4c99409ae13ae76ccc07e
Benjamin Nóbrega <95926022+Benjamin-Nobrega@users.noreply.github.com>
2024-05-17 16:18:12 -0300

    Add files via upload
    Melhoria na modificação, colocar "p1,p2,valor" no input

--- Wilson_grafo.py
+++ Wilson_grafo.py
@@ -5,11 +5,11 @@ def acharCaminho(pInicial, pFinal, matriz, indicesUsados, caminho):
     """
     Pegar caminhos diferentes
     Mostrar o caminho que pegou (feito +-)
-    
+
     """
     contador = 0
     if pInicial == pFinal:
-        
+
         return 0
     for _ in matriz[pInicial]:
 
@@ -18,30 +18,30 @@ def acharCaminho(pInicial, pFinal, matriz, indicesUsados, caminho):
         elif contador not in indicesUsados:
             indicesUsados.append(contador)
             var = acharCaminho(contador, pFinal, matriz, indicesUsados, caminho)
-            if not(var == None):
-                caminho.insert(0, contador+1)
+            if not (var == None):
+                caminho.insert(0, contador + 1)
                 return matriz[pInicial][contador] + var
         contador += 1
     return None
 
 
 def iniciarMatriz():
-    # numRange = int(input("range: "))
-    # lista = [[0 for _ in range(numRange)] for _ in range(numRange)]
-    # for i in range(numRange):
-    # for j in range(numRange):
-    # if i == j:
-    # lista[i][j] = 0
-    # else:
-    # lista[i][j] = 0
-    lista = [
-        [0, 12, 4, 0, 0, 0],
-        [0, 0, 6, 6, 0, 0],
-        [0, 0, 0, 0, 2, 0],
-        [0, 0, 8, 0, 0, 6],
-        [0, 0, 0, 0, 0, 6],
-        [0, 0, 0, 0, 0, 0],
-    ]
+    numRange = int(input("range: "))
+    lista = [[0 for _ in range(numRange)] for _ in range(numRange)]
+    for i in range(numRange):
+        for j in range(numRange):
+            if i == j:
+                lista[i][j] = 0
+            else:
+                lista[i][j] = 0
+    # lista = [
+    #     [0, 12, 4, 0, 0, 0],
+    #     [0, 0, 6, 6, 0, 0],
+    #     [0, 0, 0, 0, 2, 0],
+    #     [0, 0, 8, 0, 0, 6],
+    #     [0, 0, 0, 0, 0, 6],
+    #     [0, 0, 0, 0, 0, 0],
+    # ]
     return lista
 
 
@@ -60,13 +60,16 @@ def main():
             for i in matriz:
                 print(i)
         elif escolha == 3 and isinstance(matriz, list):
-            x = int(input("Primeiro valor: ")) - 1
-            y = int(input("Segundo valor: ")) - 1
-            if y == x:
-                print("Os índice não podem ser iguais")
-            else:
-                valor = int(input("Valor: "))
-                matriz[y][x] = valor
+            orientadoBool = input("É orientado ou não? (Y/N)").upper() == "Y"
+            arestas = int(input("Quantas arestas?\n"))
+            for i in range(arestas):
+                x, y, valor = map(int, input("Índices e valor: ").split(","))
+                if y == x:
+                    print("Os índice não podem ser iguais")
+                else:
+                    matriz[x - 1][y - 1] = valor
+                    if not orientadoBool:
+                        matriz[y-1][x-1] = valor
 
         elif escolha == 4 and isinstance(matriz, list):
             lenght = len(matriz)
@@ -80,10 +83,12 @@ def main():
         elif escolha == 6 and isinstance(matriz, list):
             pInicial = int(input("Ponto inicial: "))
             pFinal = int(input("Ponto final: "))
-            indicesUsados = []
+            indicesUsados = [pInicial]
             caminho = []
-            var = acharCaminho(pInicial - 1, pFinal - 1, matriz, indicesUsados.copy(), caminho)
-            caminho.insert(0,pInicial)
+            var = acharCaminho(
+                pInicial - 1, pFinal - 1, matriz, indicesUsados.copy(), caminho
+            )
+            caminho.insert(0, pInicial)
             if var:
                 print(f"{caminho}: {var}")
 
